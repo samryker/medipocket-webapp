148fe96c048338877b1f579020256086bcdcf4e7
Sayyam Aneja <sayyamaneja@outlook.com>
2023-01-15 17:50:55 +0530

    added http server

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 const express  = require('express')
 const app  = express()
+const http = require("http");
+
+const server = http.Server(app);
+
 const bodyParser = require("body-parser");
 const port = 4000
 
 app.use(bodyParser.json());
+app.get("/",(req,res)=>{
+    res.json("you have reached the server")
+})
 app.use("/api",require("./api"))
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server initiated at ${port}`)
 })
