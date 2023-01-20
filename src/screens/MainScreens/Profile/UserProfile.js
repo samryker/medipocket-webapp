136c3df9c8abda90793ec1f8cdec1ce77485eb98
Sayyam Aneja <sayyamaneja@outlook.com>
2023-01-20 20:26:57 +0530

    debuging user profile

--- src/screens/MainScreens/Profile/UserProfile.js
+++ src/screens/MainScreens/Profile/UserProfile.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 export const UserProfile = ({userInfo}) => {
-    const username = userInfo?.profileInfo.displayName
+    console.log(userInfo)
+    const username = userInfo?.profileInfo?.displayName
     const email = userInfo?.email
-    const countryCode = userInfo?.phoneInfo.countryCode
-    const phoneNumber = userInfo?.phoneInfo.phoneNumber
+    const countryCode = userInfo?.phoneInfo?.countryCode
+    const phoneNumber = userInfo?.phoneInfo?.phoneNumber
   return (
     <div className="user-profile-container">
         <label className="lable">Username : <span className='value'>{username}</span></label>
