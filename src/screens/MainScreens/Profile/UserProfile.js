995d3681ea8bb267de51ecbdd810097bcedc471d
Sayyam Aneja <sayyamaneja@outlook.com>
2023-01-20 20:07:10 +0530

    added null check in user profile

--- src/screens/MainScreens/Profile/UserProfile.js
+++ src/screens/MainScreens/Profile/UserProfile.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 export const UserProfile = ({userInfo}) => {
+    const username = userInfo?.profileInfo.displayName
+    const email = userInfo?.email
+    const countryCode = userInfo?.phoneInfo.countryCode
+    const phoneNumber = userInfo?.phoneInfo.phoneNumber
   return (
     <div className="user-profile-container">
-        <label className="lable">Username : <span className='value'>{userInfo.profileInfo.displayName}</span></label>
-        <label className="lable">Email : <span className='value'>{userInfo.email}</span></label>
-        <label className="lable">Phone Number : <span className='value'>+{userInfo.phoneInfo.countryCode} {userInfo.phoneInfo.phoneNumber}</span></label>
+        <label className="lable">Username : <span className='value'>{username}</span></label>
+        <label className="lable">Email : <span className='value'>{email}</span></label>
+        <label className="lable">Phone Number : <span className='value'>{phoneNumber?`+${countryCode} ${phoneNumber}`:''}</span></label>
     </div>
   )
 }
