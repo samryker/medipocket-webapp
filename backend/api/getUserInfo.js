879c29244da77eccc363161d767e40cdd06c2c58
Sayyam Aneja <sayyamaneja@outlook.com>
2023-01-20 11:56:17 +0530

    added console logs in backend

--- backend/api/getUserInfo.js
+++ backend/api/getUserInfo.js
@@ -3,10 +3,12 @@ const Controller = require("../controllers")
 async function getAccessToken(req,res){
     try{
         const {data} = req.body
+        console.log(data)
         const accessTokenResponse = await Controller.getAccessToken(data.authId)
         console.log(accessTokenResponse)
         if(!accessTokenResponse.error){
             const userInfo = await Controller.getUserInfo(accessTokenResponse.access_token)
+            console.log(userInfo)
             res.json(userInfo)
         }
     }catch(error){
