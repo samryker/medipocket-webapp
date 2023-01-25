14232821f0abe4a9aa7cce73ea4f07341cccc1db
Sayyam Aneja <sayyamaneja@outlook.com>
2023-01-25 22:29:20 +0530

    styled user profile

--- src/screens/MainScreens/Profile/UserProfile.js
+++ src/screens/MainScreens/Profile/UserProfile.js
@@ -1,21 +1,30 @@
-import React from 'react'
-import './stylesProfile.css'
+import React from "react";
+import "./stylesProfile.css";
 
-export const UserProfile = ({userInfo}) => {
-    console.log(userInfo)
-    const username = userInfo?.profileInfo?.displayName
-    const email = userInfo?.email
-    const countryCode = userInfo?.phoneInfo?.countryCode
-    const phoneNumber = userInfo?.phoneInfo?.phoneNumber
+export const UserProfile = ({ userInfo }) => {
+  console.log(userInfo);
+  const username = userInfo?.profileInfo?.displayName ?? "name";
+  const email = userInfo?.email ?? "email";
+  const countryCode = userInfo?.phoneInfo?.countryCode ?? "cc";
+  const phoneNumber = userInfo?.phoneInfo?.phoneNumber ?? "phone-number";
+
   return (
-    <div className="user-profile-container" >
-      <div className="profile-title-container">
-        <p className="profile-title" >Profile</p>
+    <div className="user-profile-container">
+      <p className="profile-title">Profile</p>
+      <div className="user-info">
+        <label className="lable">
+          Username<span className="value">{username}</span>
+        </label>
+        <label className="lable">
+          Email<span className="value">{email}</span>
+        </label>
+        <label className="lable">
+          Phone Number
+          <span className="value">
+            {phoneNumber ? `+${countryCode} ${phoneNumber}` : ""}
+          </span>
+        </label>
       </div>
-      
-        <label className="lable">Username : <span className='value'>{username}</span></label>
-        <label className="lable">Email : <span className='value'>{email}</span></label>
-        <label className="lable">Phone Number : <span className='value'>{phoneNumber?`+${countryCode} ${phoneNumber}`:''}</span></label>
     </div>
-  )
-}
+  );
+};
