df78aa73fb7d4fc9d1641324f2d415c805132a60
Sayyam Aneja <sayyamaneja@outlook.com>
2023-01-28 17:58:58 +0530

    added mandatory consent popup

---
+++ src/screens/Components/SkipPopup.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const SkipPopup = ({loginFlow}) => {
+  const message = "Granting consent is necessary for proceeding with the flow.";
+  function handleExit(){
+    window.JSBridge.call('popWindow');
+  }
+  function handleRetry(){
+    loginFlow()
+  }
+  return (
+    <div className="popupContainer">
+      <div className="popup">
+        <div className="popupMessage">
+          <p>{message}</p>
+          <div className="actions">
+            <button onClick={handleExit} className="exit">EXIT</button>
+            <button onClick = {handleRetry} className="retry">RETRY</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
